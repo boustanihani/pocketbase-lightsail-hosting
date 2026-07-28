115157a54e8f0188af1c403210eb20a73d3ae7f3
boustanihani <boustanihani@users.noreply.github.com>
2026-07-28 21:10:37 +0200

    Add journalctl commands for earlyoom and OOM logs

--- misc-commands.sh
+++ misc-commands.sh
@@ -7,5 +7,7 @@ sudo cat /var/log/cloud-init-output.log | curl -s -F "content=<-" https://dpaste
 
 sudo cat /myapps/filebrowser/filebrowser.err.log | grep -i password
 
+journalctl -u earlyoom --since "2 hours ago" # logs "sending SIGTERM to process NNNN"
+journalctl -k | grep -i -E 'oom|killed process' # kernel OOM killer, if it got that far
 
 
